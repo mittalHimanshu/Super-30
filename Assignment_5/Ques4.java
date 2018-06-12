f88623d80d4891b2e795c36a725a7a965f201dad
Himanshu Mittal <mittalhimanshu121@gmail.com>
2018-06-12 15:28:15 +0530

    updated ques4

--- Assignment_5/Ques4.java
+++ Assignment_5/Ques4.java
@@ -6,15 +6,15 @@ public class Ques4 {
     public static void printWaveform(int arr[], int size){
         for(int i=0;i<size;i++)
 	     {
-            for(int j=i+1;j<size;j++)
-            {
-		         if(arr[i]>arr[j])
-		         {
-                    int temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
+            	for(int j=i+1;j<size;j++)
+            	{
+		 	if(arr[i]>arr[j])
+		        {
+                    		int temp=arr[i];
+                    		arr[i]=arr[j];
+                    		arr[j]=temp;
 		         }
-            }	
+            	}	
         }
         for(int i=0; i<size; i+=2){
             int temp = arr[i];
