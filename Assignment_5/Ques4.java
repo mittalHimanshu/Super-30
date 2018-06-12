19540cc4499ed6afde38b517ad09067101fba84a
Himanshu Mittal <mittalhimanshu121@gmail.com>
2018-06-12 15:29:25 +0530

    updated ques4

--- Assignment_5/Ques4.java
+++ Assignment_5/Ques4.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Ques4 {
     public static void printWaveform(int arr[], int size){
         for(int i=0;i<size;i++)
-	     {
-            	for(int j=i+1;j<size;j++)
+	{
+        	for(int j=i+1;j<size;j++)
             	{
 		 	if(arr[i]>arr[j])
 		        {
