7fd09e60eec27c45924dc63ebc6dc819848cbf54
Himanshu Mittal <mittalhimanshu151@gmail.com>
2018-06-20 23:05:36 +0530

    file listing app updated

--- Assignment_11/FileListingApp.java
+++ Assignment_11/FileListingApp.java
@@ -1,5 +1,7 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -8,7 +10,7 @@ public class FileListingApp{
     
     public static void Files(String path, ArrayList al) throws IOException {
 	File f=new File(path);
-	try{
+		try{
             File f1[]=f.listFiles();
             for(File file:f1){
                 if(file.isDirectory() == true){
@@ -34,38 +36,45 @@ public class FileListingApp{
 	System.out.println("Enter path of File/Directory : ");
 	String address = xyz.nextLine();
         System.out.println("Enter path of csv file : ");
-	String address1 = xyz.nextLine();
+		String address1 = xyz.nextLine();
         xyz.close();
         PrintWriter print = new PrintWriter("paths.txt");
         print.append(address);
         print.println();
         print.append(address1);
         print.flush();
-	ArrayList<File> arrayList=new ArrayList<>();
+        print.close();
+        String location[] = new String[2];
+        int j = 0;
+		ArrayList<File> arrayList=new ArrayList<>();
         PrintWriter out = null;
-	try{
-            Files(address,arrayList);
-            File f = new File(address1);
+		try{
+            File f1 = new File("paths.txt");
+            BufferedReader br = new BufferedReader(new FileReader(f1));
+            String line;
+            while ((line = br.readLine()) != null) {
+                location[j] = line;
+                j++;
+            }
+            Files(location[0],arrayList);
+            File f = new File(location[1]);
             f.createNewFile();
             out = new PrintWriter(new FileOutputStream(f, true));
             out.append("Name of File,Path of File\n\r");
             for(int i=0;i<arrayList.size();i++){
                 out.append(arrayList.get(i).getName() + ",");
-		out.append(arrayList.get(i).getAbsolutePath());
-		out.println();
-		out.flush();
+				out.append(arrayList.get(i).getAbsolutePath());
+				out.println();
+				out.flush();
             }
-	}
+		}
         catch(IOException e){
             System.out.println(e.getMessage());
-	}
+		}
         finally {
             if(out != null){
                 out.close();
             }
-            if(print != null){
-                print.close();
-            }
-	}
+		}
     }
 }
