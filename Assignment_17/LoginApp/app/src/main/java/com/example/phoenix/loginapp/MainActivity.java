ff9960abc3056f695c43cb9d87e4758ec1e790b4
Himanshu Mittal <mittalhimanshu151@gmail.com>
2018-06-28 16:34:23 +0530

    updated assignment 17

--- Assignment_17/LoginApp/app/src/main/java/com/example/phoenix/loginapp/MainActivity.java
+++ Assignment_17/LoginApp/app/src/main/java/com/example/phoenix/loginapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,16 +27,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 name = name1.getText().toString();
-                address = address1.getText().toString();
-                email = email1.getText().toString();
-                phone = phone1.getText().toString();
                 password = password1.getText().toString();
-                Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("Username", name);
-                bundle.putString("Password", password);
-                i.putExtras(bundle);
-                startActivity(i);
+                if(name.equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter Username", Toast.LENGTH_SHORT).show();
+                }
+                else if(password.equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter Password", Toast.LENGTH_SHORT).show();
+                }
+                else if(name.equals("") && password.equals("")){
+                    Toast.makeText(getApplicationContext(), "Enter Username/Password", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    address = address1.getText().toString();
+                    email = email1.getText().toString();
+                    phone = phone1.getText().toString();
+                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("Username", name);
+                    bundle.putString("Password", password);
+                    i.putExtras(bundle);
+                    startActivity(i);
+                }
             }
         });
     }
